ad46b6ed2c1de3e4dfb25443daf94339f26261ee
Alexey <alexey.nebolsin.jr@gmail.com>
2020-05-11 16:19:27 +0300

    mysave

---
+++ Client-summ/Notifies/notifyshower.cpp
@@ -0,0 +1,44 @@
+#include "notifyshower.h"
+using namespace Mafia;
+
+NotifyShower::NotifyShower(QWidget *parent) : QLabel(parent)
+{
+    this->setStyleSheet("background-color: #BBBBBB;"
+                        "font-size: 24px;");
+    this->setAlignment(Qt::AlignCenter);
+}
+
+void NotifyShower::dayKill(int idx) {
+    msg = "Игрок "+QString::number(idx)+" убит";
+}
+
+void NotifyShower::nominate(int idx) {
+    msg = "Игрок "+QString::number(idx)+" выдвинут";
+}
+
+void NotifyShower::sheriffResult(int idx, bool res) {
+    msg = "Игрок "+QString::number(idx)+" ";
+    if(res) msg += "мафия";
+    else msg += "мирный";
+}
+
+void NotifyShower::updateBounds(QSize nbounds) {
+    parBounds = nbounds;
+}
+
+void NotifyShower::updateRelatives(QList<double> rels) {
+    myDimens = rels;
+}
+
+void NotifyShower::showMessage() {
+    QRect geom = QRect(myDimens[0]*parBounds.width(), myDimens[1]*parBounds.height(), myDimens[2]*parBounds.width(), myDimens[3]*parBounds.height());
+    this->setGeometry(geom);
+    this->setText(msg);
+    this->show();
+    this->raise();
+    QTimer::singleShot(delay, this, &NotifyShower::closeMessage);
+}
+
+void NotifyShower::closeMessage() {
+    this->close();
+}
