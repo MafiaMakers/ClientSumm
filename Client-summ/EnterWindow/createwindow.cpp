48ef3c4016ec887dd233e43e284fd8aac69f06d7
Alexey <alexey.nebolsin.jr@gmail.com>
2020-05-13 17:46:57 +0300

    come in fix

--- Client-summ/EnterWindow/createwindow.cpp
+++ Client-summ/EnterWindow/createwindow.cpp
@@ -9,29 +9,29 @@ MyCreateWindow::MyCreateWindow(QWidget *parent): QDialog(parent)
     this->setWindowTitle("Create new game");
 
     RoomId_label = new QLabel(this);
-    RoomId_label->setGeometry(120, 10, 80, 30);
+    RoomId_label->setGeometry(120, 10, 400, 30);
     RoomId_label->setText("Room Id:");
     RoomId_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     RoomId_label->setTextInteractionFlags(Qt::TextSelectableByMouse);
     RoomId = new QLabel(this);
-    RoomId->setGeometry(200, 10, 30, 30);
+    RoomId->setGeometry(220, 10, 30, 30);
     RoomId->setTextInteractionFlags(Qt::TextSelectableByMouse);
     RoomId->setFont(QFont("MS Shell Dlg 2", 12));
 
 
     RoomKey_label = new QLabel(this);
-    RoomKey_label->setGeometry(10, 50, 90, 30);
+    RoomKey_label->setGeometry(10, 50, 400, 30);
     RoomKey_label->setText("Room Key:");
     RoomKey_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     RoomKey_label->setTextInteractionFlags(Qt::TextSelectableByMouse);
     RoomKey = new QLabel(this);
-    RoomKey->setGeometry(110, 50, 280, 30);
+    RoomKey->setGeometry(130, 50, 280, 30);
     RoomKey->setTextInteractionFlags(Qt::TextSelectableByMouse);
     RoomKey->setFont(QFont("MS Shell Dlg 2", 12));
 
 
     CopyButton = new QPushButton(this);
-    CopyButton->setGeometry(150, 90, 80, 30);
+    CopyButton->setGeometry(160, 90, 80, 30);
     CopyButton->setText("Copy");    
 
     Line = new QFrame(this);
@@ -40,19 +40,19 @@ MyCreateWindow::MyCreateWindow(QWidget *parent): QDialog(parent)
     Line->setFrameShadow(QFrame::Sunken);
 
     Name_label = new QLabel(this);
-    Name_label->setGeometry(10, 170, 90, 30);
+    Name_label->setGeometry(10, 170, 400, 30);
     Name_label->setText("Your name");
     Name_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     Name = new QLineEdit(this);
-    Name->setGeometry(110, 170, 270, 30);
+    Name->setGeometry(120, 170, 270, 30);
     Name->setFont(QFont("MS Shell Dlg 2", 12));
 
     CameraOn = new QCheckBox(this);
-    CameraOn->setGeometry(30, 220, 240, 20);
+    CameraOn->setGeometry(30, 220, 400, 20);
     CameraOn->setText("Don't turn on my camera");
     CameraOn->setFont(QFont("MS Shell Dlg 2", 12));
     MicroOn = new QCheckBox(this);
-    MicroOn->setGeometry(30, 260, 240, 20);
+    MicroOn->setGeometry(30, 260, 400, 20);
     MicroOn->setText("Don't turn on my microphone");
     MicroOn->setFont(QFont("MS Shell Dlg 2", 12));
 
@@ -64,6 +64,10 @@ MyCreateWindow::MyCreateWindow(QWidget *parent): QDialog(parent)
     connect(OkCancelBox, &QDialogButtonBox::accepted, this, &MyCreateWindow::OkButton);
     connect(OkCancelBox, &QDialogButtonBox::rejected, this, &MyCreateWindow::cancel);
     connect(CopyButton, &QPushButton::clicked, this, &MyCreateWindow::Copy);
+
+    RoomId_label->raise();
+    RoomKey_label->raise();
+    Name_label->raise();
 }
 
 void MyCreateWindow::setup(QString key, int roomId){
--- Client-summ/EnterWindow/enterwindow.cpp
+++ Client-summ/EnterWindow/enterwindow.cpp
@@ -9,40 +9,40 @@ EnterWindow::EnterWindow(QWidget *parent): QDialog(parent)
     this->setWindowTitle("Enter game");
 
     RoomId_label = new QLabel(this);
-    RoomId_label->setGeometry(170, 40, 80, 30);
+    RoomId_label->setGeometry(170, 40, 400, 30);
     RoomId_label->setText("Room Id");
     RoomId_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     RoomId = new QSpinBox(this);
-    RoomId->setGeometry(250, 40, 50, 30);
+    RoomId->setGeometry(260, 40, 50, 30);
     RoomId->setFont(QFont("MS Shell Dlg 2", 12));
 
     RoomKey_label = new QLabel(this);
-    RoomKey_label->setGeometry(20, 100, 90, 30);
+    RoomKey_label->setGeometry(20, 100, 400, 30);
     RoomKey_label->setText("Room Key");
     RoomKey_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     RoomKey = new QLineEdit(this);
-    RoomKey->setGeometry(120, 100, 270, 30);
+    RoomKey->setGeometry(130, 100, 270, 30);
     RoomKey->setFont(QFont("MS Shell Dlg 2", 12));
 
     PasteButton = new QPushButton(this);
-    PasteButton->setGeometry(400, 100, 70, 30);
+    PasteButton->setGeometry(410, 100, 70, 30);
     PasteButton->setText("Paste");
     PasteButton->setFont(QFont("MS Shell Dlg 2", 12));
 
     Name_label = new QLabel(this);
-    Name_label->setGeometry(20, 160, 90, 30);
+    Name_label->setGeometry(20, 160, 400, 30);
     Name_label->setText("Your name");
     Name_label->setFont(QFont("MS Shell Dlg 2", 12, QFont::Bold));
     Name = new QLineEdit(this);
-    Name->setGeometry(120, 160, 350, 30);
+    Name->setGeometry(130, 160, 350, 30);
     Name->setFont(QFont("MS Shell Dlg 2", 12));
 
     CameraOn = new QCheckBox(this);
-    CameraOn->setGeometry(30, 220, 240, 20);
+    CameraOn->setGeometry(30, 220, 400, 20);
     CameraOn->setText("Don't turn on my camera");
     CameraOn->setFont(QFont("MS Shell Dlg 2", 12));
     MicroOn = new QCheckBox(this);
-    MicroOn->setGeometry(30, 260, 240, 20);
+    MicroOn->setGeometry(30, 260, 400, 20);
     MicroOn->setText("Don't turn on my microphone");
     MicroOn->setFont(QFont("MS Shell Dlg 2", 12));
 
@@ -50,6 +50,11 @@ EnterWindow::EnterWindow(QWidget *parent): QDialog(parent)
     OkCancelBox->setGeometry(170, 310, 160, 20);
     OkCancelBox->addButton(QDialogButtonBox::Ok);
     OkCancelBox->addButton(QDialogButtonBox::Cancel);
+
+    RoomId_label->raise();
+    RoomKey_label->raise();
+    Name_label->raise();
+    RoomId_label->raise();
 
     connect(OkCancelBox, &QDialogButtonBox::accepted, this, &EnterWindow::OkButton);
     connect(OkCancelBox, &QDialogButtonBox::rejected, this, &EnterWindow::cancel);
